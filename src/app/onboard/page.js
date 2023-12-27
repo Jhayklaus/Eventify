472e3f6abb3eff0ce1c44d7a297005f2c656e8db
jhayklaus <jamiumoyosore02@gmail.com>
2023-12-27 11:57:02 +0100

    add: onboarding and login screens

---
+++ src/app/onboard/page.js
@@ -0,0 +1,12 @@
+import Onboard from "@/components/onboard/Onboard";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Onboard />
+    </div>
+  );
+};
+
+export default page;
